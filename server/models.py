4021ae2ea5cb92383b795af0983cd96ff8025e3a
Racheal <rachealnjugush@gmail.com>
2025-01-31 08:19:37 +0300

    done

--- server/models.py
+++ server/models.py
@@ -7,9 +7,14 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
-
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    _password_hash = db.Column(db.String(128), nullable = False)
+    image_url = db.Column(db.String(255), nullable=True)
+    bio = db.Column(db.String(255), nullable=True)
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
-    
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable = False)
+    instructions = db.Column(db.Text, nullable = False)
+    minutes_to_complete = db.Column(db.Integer, nullable = False)
